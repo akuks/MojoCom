2d33408962adc497b56896b64c8e10651b903d80
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-10 17:53:49 +0530

    Entry point to start the application and containers

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,9 +2,25 @@
 
 set -e
 
-# Install Mojolicious
+echo "Setting up"
+
+## SETUP
 MORBO="/usr/local/bin/morbo"
 APP_BIN="/opt/MojoCom/script/mojo_com"
+FE_APP="/opt/MojoCom/app/"
+
+## Change Directory
+echo "Change the directory"
+cd $FE_APP
+#
+#which yarn
+
+## Start the Frontend
+echo "Starting the Application"
+yarn install
+
+yarn start &
 
 # Start the Backend Application
 $MORBO $APP_BIN
+#/usr/local/bin/morbo /opt/MojoCom/script/mojo_com
