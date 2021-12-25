5f2687410870cc41db0fa3098cec577fd6ae9d1c
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-25 12:38:11 +0530

    Create start.sh script

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,28 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-PERL_BINARY="/usr/local/Cellar/perl/5.34.0/bin/perl"
+set -e
+
+
+echo "Setting up the application"
+
+## SETUP
+PERL_BINARY="perl"
+
+BASE_DIR=`pwd`
+LIB_PATH="$BASE_DIR/local/lib/perl5/"
+
+MORBO_BINARY="$BASE_DIR/local/bin/morbo"
+
+APP_BIN="$BASE_DIR/script/mojo_com"
+
+TEMPLATE="$BASE_DIR/templates/"
+
+
+echo "Export Perl Library Path"
+export PERL5LIB=$LIB_PATH
+
+# shellcheck disable=SC2028
+echo "Starting the application\n"
+
+carton exec $PERL_BINARY -I $LIB_PATH $MORBO_BINARY $APP_BIN -l http://*:3000
 
