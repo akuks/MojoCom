ee914274ad1ce505859d864b28c3c6dbc6d8c518
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-26 13:39:24 +0530

    Add postgres DB Plugin

---
+++ lib/MojoCom/Plugin/DB.pm
@@ -0,0 +1,24 @@
+package MojoCom::Plugin::DB;
+use Mojo::Base 'Mojolicious::Plugin', -signatures;
+
+use warnings;
+use MojoCom::Schema;
+use DBIx::Class();
+
+use strict;
+
+sub register ( $self, $app, $conf ) {
+
+    my $config = $app->plugin('NotYAMLConfig');
+    my $db_config = $config->{ database }->{ $app->mode };
+
+    my $dbh = $config->{ database }->{ schema }->connect(
+        $db_config->{ dsn },
+        $db_config->{ username },
+        $db_config->{ password }
+    );
+
+    return $dbh;
+}
+
+1;
