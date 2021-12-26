555cfeae47277884dd4dfbc08f5303aee4e8bd58
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2021-12-26 23:22:59 +0530

    Hack response and modify the output to JSON

--- lib/MojoCom.pm
+++ lib/MojoCom.pm
@@ -5,33 +5,29 @@ use MojoCom::Plugin::DB;
 # This method will run once at server start
 sub startup ($self) {
 
-  # Load configuration from config file
-  my $config = $self->plugin('NotYAMLConfig');
+    # Load configuration from config file
+    my $config = $self->plugin('NotYAMLConfig');
 
-  $self->plugin( 'AutoReload' );
+    $self->plugin( 'AutoReload' );
 
-  # Configure the application
-  $self->secrets($config->{secrets});
+    # Configure the application
+    $self->secrets($config->{secrets});
 
-  $self->plugin("OpenAPI" => {
-      url => $self->home->rel_file("api.yaml"), schema => "v3"
-  });
+    $self->plugin("OpenAPI" => {
+        url => $self->home->rel_file("api.yaml"), schema => "v3"
+    });
 
-  $self->helper( dbh => $self->plugin('MojoCom::Plugin::DB') );
+    $self->helper( dbh => $self->plugin('MojoCom::Plugin::DB') );
 
-  # $self->plugin(
-  #     OpenAPI => {
-  #         renderer => sub {
-  #           my ($c, $data) = @_;
-  #           return ($data);
-  #         }
-  #     }
-  # );
+    # Workaround
+    $self->hook(
+        after_dispatch => sub ( $c ) {
+            my $response = (  $c->res ) ?  $c->res->{ content }->asset->{content} : $c->response;
+            my @query = split(/<style>/, $response );
+            $c->render( openapi => json => Mojo::JSON::decode_json ( $query[0] ) )
+        }
+    );
 
-}
-
-sub return_response ( $self, $c, $output ) {
-    $c->render( openapi => json => $output )
 }
 
 1;
