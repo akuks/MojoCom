3b05358c6e6d65e207fd16a0b1d2bcbf151b53b6
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-02 20:19:07 +0530

    Add User Schema Resultset

--- lib/MojoCom/Schema/ResultSet/User.pm
+++ lib/MojoCom/Schema/ResultSet/User.pm
@@ -23,4 +23,23 @@ sub is_user_exists ( $self, $email ) {
     return $self->find( {  email => $email } );
 }
 
+# Find user by unique user key
+sub find_user_by_user_key ( $self, $user_key ) {
+    return $self->find( {  user_key => $user_key } );
+}
+
+# Find user by user_id
+sub find_user_by_id ( $self, $user_id ) {
+    return $self->find( { user_id => $user_id } );
+}
+
+sub validate_user_cred ( $self, $email, $password ) {
+    my $user;
+    unless ( $user = $self->is_user_exists( $email ) ) {
+        return 0
+    }
+
+    return $user;
+}
+
 1;
