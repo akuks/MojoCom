52e659204cf29938ce8a75242da5c012a9233c38
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-02 20:18:46 +0530

    Add Login API

--- lib/MojoCom/Controller/Auth/Login.pm
+++ lib/MojoCom/Controller/Auth/Login.pm
@@ -8,7 +8,24 @@ use JSON;
 sub login( $c ) {
     my $app = $c->openapi->valid_input or return;
 
-    my $output = { message => 'Login Successful' } ;
+    my $email = $c->param( 'username' );
+    my $password = $c->param( 'password' );
+
+    my $user = $c->app->dbh->resultset( 'User')->validate_user_cred( $email, $password );
+
+    unless ( $c->bcrypt_validate( $password || '', $user->password) ) {
+        return $c->render(openapi => { error => 'Invalid username or password.', status => 400 } )
+    }
+
+    my $output = {
+        message => 'Login Successful',
+        token   =>  $c->app->jwt_encode(
+            {
+                id => $user->id,
+                secret => $c->app->plugin('NotYAMLConfig')->{api_key}
+            }
+        )
+    } ;
 
     $app->render( openapi => json => $output )
 }
