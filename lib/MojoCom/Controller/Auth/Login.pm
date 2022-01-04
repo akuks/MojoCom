8a2ba2da6ecbbaf65a9f5d93a659a4babfb0734e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-04 15:26:04 +0530

    Login API

--- lib/MojoCom/Controller/Auth/Login.pm
+++ lib/MojoCom/Controller/Auth/Login.pm
@@ -17,17 +17,43 @@ sub login( $c ) {
         return $c->render(openapi => { error => 'Invalid username or password.', status => 400 } )
     }
 
+    my $jwt = $c->app->jwt_encode(
+        {
+            id     => $user->user_key,
+            secret => $c->app->plugin('NotYAMLConfig')->{api_key}
+        }
+    );
+
     my $output = {
         message => 'Login Successful',
-        token   =>  $c->app->jwt_encode(
-            {
-                id => $user->id,
-                secret => $c->app->plugin('NotYAMLConfig')->{api_key}
-            }
-        )
+        token   =>  $jwt->encode,
+        expires =>  $jwt->expires
     } ;
 
     $app->render( openapi => json => $output )
+}
+
+sub profile ( $c ) {
+    my $app = $c->openapi->valid_input or return;
+
+    my $user = $c->app->dbh->resultset( 'User' )->find_user_by_uuid( $c->req->headers->header('user') );
+
+    my @user = map {
+        {
+            email        => $_->email,
+            first_name   => $_->first_name,
+            middle_name  => $_->middle_name,
+            last_name    => $_->last_name,
+            url_blod     => $_->url_blog,
+            url_linkedin => $_->url_linkedin,
+            url_github   => $_->url_github,
+            created_at   => $_->created_at,
+            updated_at   => $_->updated_at,
+            user_key     => $_->user_key
+        }
+    } $user;
+
+    $app->render( openapi => { user => $user[0] } )
 }
 
 sub list ( $c ) {
