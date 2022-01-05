684a64f8434a63a0eddafd8bb57332b3b95d2ec2
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-05 12:12:53 +0530

    Validation added for names and urls

--- lib/MojoCom/Plugin/Validation.pm
+++ lib/MojoCom/Plugin/Validation.pm
@@ -9,12 +9,23 @@ sub register ( $c, $app, $conf ) {
 
     $app->helper( param_validation => sub ( $c ) {
         return state $param_validation = {
-            username => sub ( $v, $email ) {
-                $v->required( 'username' )->like( Email::Valid->address( $email ) )
+            username => sub( $v, $email ) {
+                $v->required('username')->like(Email::Valid->address($email))
             },
-            password => sub ( $v ) {
-                $v->required( 'password' )->like( qr/^[a-zA-Z0-9]+$/ );
-                $v->required( 'confirm_password' )->equal_to( 'password' ) if $v->required( 'password' )->is_valid;
+            password => sub( $v ) {
+                $v->required('password')->like(qr/^[a-zA-Z0-9]+$/);
+                $v->required('confirm_password')->equal_to('password') if $v->required('password')->is_valid;
+            },
+            name     => sub( $v, $name ) {
+                if ($name =~ m/^first_|^last_/) {
+                    $v->required( $name )->like(qr/^[a-zA-Z]/)
+                }
+                else {
+                    $v->optional( $name )->like(qr/^$|^[a-zA-Z]/);
+                }
+            },
+            url      => sub ( $v, $url ) {
+                $v->optional( $url )->like(qr/^$|^(?:(?:https?|s?))/);
             }
         }
     });
