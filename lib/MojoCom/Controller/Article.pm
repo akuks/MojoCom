e3076776d8e0e0c75f1033b7558eaf44a1a3a762
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-18 23:48:13 +0530

    ParamValidation Logic added

--- lib/MojoCom/Controller/Article.pm
+++ lib/MojoCom/Controller/Article.pm
@@ -1,13 +1,17 @@
 package MojoCom::Controller::Article;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
+use MojoCom::ParamValidation;
 
 # This action will render a template
 sub create ( $c ) {
     my $app = $c->openapi->valid_input or return;
 
-    my $v = $c->validation;
+    my $v = MojoCom::ParamValidation->new( 
+        validator        => $c->validation, 
+        param_validation => $c->param_validation 
+    )->validate_parameters;
   
-    $app->render( openapi => json => 'Article created successfully' )
+    $app->render( openapi => { message => 'Article created successfully' } )
 }
 
 1;
