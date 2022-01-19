2cff0acbae47be4c9a132abac9b67b4caf1b8e5a
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-19 23:09:59 +0530

    Pass a list to validate the parameters

--- lib/MojoCom/ParamValidation.pm
+++ lib/MojoCom/ParamValidation.pm
@@ -2,17 +2,18 @@ package MojoCom::ParamValidation;
 use Mojo::Base -base, -signatures;
 
 has 'validator';
-
 has 'param_validation' => sub { die 'param_validation is required' };
+has 'param_list' => sub { die 'param_list is required' };
 
 sub validate_parameters ( $self ) {
     my $validator = $self;
-    my $input = $self->validator->input;
+    my $input = $self->param_list;
     
-    foreach ( keys %$input ) {
-        $c->param_validation->{ $_ }->( $self->validator );
+    foreach ( @$input ) {
+        $self->param_validation->{ $_ }->( $self->validator );
     }
 
+    return $self->validator;
 }
 
 
