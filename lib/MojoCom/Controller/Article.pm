237e6e3eedad450abb3dcaf9ba5d58b1af6751cf
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-19 23:10:21 +0530

    Update the Article to verify the parameters

--- lib/MojoCom/Controller/Article.pm
+++ lib/MojoCom/Controller/Article.pm
@@ -5,12 +5,17 @@ use MojoCom::ParamValidation;
 # This action will render a template
 sub create ( $c ) {
     my $app = $c->openapi->valid_input or return;
-
+    
+    my @list = ['title', 'body', 'category']; # Required Parameters
+    
     my $v = MojoCom::ParamValidation->new( 
         validator        => $c->validation, 
-        param_validation => $c->param_validation 
+        param_validation => $c->param_validation ,
+        param_list       => @list
     )->validate_parameters;
-  
+
+    return $app->render( openapi => { error => 'Invalid Input Parameters' } ) if $v->has_error;
+
     $app->render( openapi => { message => 'Article created successfully' } )
 }
 
