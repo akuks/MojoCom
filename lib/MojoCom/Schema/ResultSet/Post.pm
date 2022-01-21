572664f78f82a674de48bd68f9f88496b9e0033e
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-22 00:18:12 +0530

    UPdate resultset get article by slug subroutine

--- lib/MojoCom/Schema/ResultSet/Post.pm
+++ lib/MojoCom/Schema/ResultSet/Post.pm
@@ -14,8 +14,11 @@ sub create_or_update_article ( $self, $options ) {
             { key => 'posts_slug_user_key' }
         );
     };
-    print $@;
     return $@ ? 0 : $article;
+}
+
+sub get_article_by_slug( $self, $slug, $id ) {
+    return $self->find({ slug => $slug, id => $id});
 }
 
 
