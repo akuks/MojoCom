936a9057256186b148c740a4b8bb347f9f760760
Ashutosh Kukreti <kukreti.ashutosh@gmail.com>
2022-01-27 11:27:39 +0530

    Create editor class

---
+++ public/js/MojoComEditorConfig.js
@@ -0,0 +1,130 @@
+/**
+* This Class is to Set the editorjs configuration settings
+ */
+class MojoComEditorConfig {
+    constructor(editor, csrf_token) {
+        this.editor = editor;
+        this.csrf_token = csrf_token;
+    }
+
+    get editorConfig() {
+        return new EditorJS({
+            /**
+            * Enable/Disable the read only mode
+            */
+            readOnly: false,
+
+            /**
+            * Wrapper of Editor
+            */
+            holder: this.editor,
+
+            // Set Autofocus true
+            autofocus: true,
+
+            /**
+            * Tools list
+            */
+            tools: {
+                // Set Headers
+                header: {
+                    class: Header,
+                    inlineToolbar: ['link', 'marker', 'bold', 'italic'],
+                    config: {
+                        placeholder: 'Title of the Article'
+                    },
+                    shortcut: 'CMD+SHIFT+H'
+                },
+
+                anyTuneName: {
+                    class: AlignmentBlockTune,
+                    config: {
+                        placeholder: 'Alignment',
+                        default: "left",
+                        blocks: {
+                            header: 'center',
+                            list: 'right'
+                        }
+                    },
+                },
+
+                /**
+                * Or pass class directly without any configuration
+                */
+                image: {
+                    class: ImageTool,
+                    config: {
+                            additionalRequestHeaders: {
+                            "X-CSRF-TOKEN": this.csrf_token
+                        },
+                        endpoints: {
+                            byFile: 'http://localhost.0x484c.com:3000/upload/image'
+                        }
+                    }
+                },
+
+                list: {
+                    class: List,
+                    inlineToolbar: true,
+                    shortcut: 'CMD+SHIFT+L'
+                },
+
+                checklist: {
+                    class: Checklist,
+                    inlineToolbar: true,
+                },
+
+                quote: {
+                    class: Quote,
+                    inlineToolbar: true,
+                    config: {
+                        quotePlaceholder: 'Enter a quote',
+                        captionPlaceholder: 'Quote\'s author',
+                    },
+                    shortcut: 'CMD+SHIFT+O'
+                },
+
+                marker: {
+                    class: Marker,
+                    shortcut: 'CMD+SHIFT+M'
+                },
+
+                code: {
+                    class: CodeTool,
+                    shortcut: 'CMD+SHIFT+C'
+                },
+
+                inlineCode: {
+                    class: InlineCode,
+                    shortcut: 'CMD+SHIFT+C'
+                },
+
+                linkTool: LinkTool,
+
+                embed: Embed,
+
+                table: {
+                    class: Table,
+                    inlineToolbar: true,
+                    shortcut: 'CMD+ALT+T'
+                },
+
+            },
+
+            /**
+            * Initial Editor data
+            */
+            data: {
+                blocks: [
+                    {
+                        type: "header",
+                        data: {
+                            text: "Post Title...",
+                            level: 2
+                        }
+                    }
+                ]
+            },
+        });
+    }
+}
